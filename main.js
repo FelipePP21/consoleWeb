c252f830ede2a343117af89d9a2e5180e72d7d85
FelipePP21 <131295218+FelipePP21@users.noreply.github.com>
2024-03-27 14:52:31 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("meu primeiro projeto no console");
+
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+
+var fullName = first_name.concat(last_name);
+
+console.log("fullName");
+
+var a = 10;
+var b = 5;
+var add = a + b;
+
+console.log("add");
